406590ca38c249d9b1ee62b4f71aed5a8a0a665b
Marcin Pietrzak <marcin@iworks.pl>
2026-05-07 17:21:58 +0200

    fix

--- assets/scripts/src/admin/dashboard.js
+++ assets/scripts/src/admin/dashboard.js
@@ -1,17 +1,16 @@
-
-jQuery(document).ready(function ($) {
-    $('#iworks-aqualog-dashboard-message').on('click', '.notice-dismiss', function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: ajaxurl,
-            type: 'POST',
-            data: {
-                action: 'iworks_aqualog_dismiss_message',
-                nonce: '<?php echo wp_create_nonce( 'iworks_aqualog_dismiss_message' ); ?>'
-					},
-            success: function (response) {
-                $('#iworks-aqualog-dashboard-message').fadeOut();
-            }
-        });
-    });
+jQuery(document).ready(function($) {
+	$('#iworks-aqualog-dashboard-message').on('click', '.notice-dismiss', function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: ajaxurl,
+			type: 'POST',
+			data: {
+				action: 'iworks_aqualog_dismiss_message',
+				_wpnonce: window.aqualog.nonces.dismiss_message
+			},
+			success: function(response) {
+				$('#iworks-aqualog-dashboard-message').fadeOut();
+			}
+		});
+	});
 });
